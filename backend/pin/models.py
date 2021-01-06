ad98245fb4c4e701412828ba5d1b0af5cebd8259
hyeseong <hystom38@gmail.com>
2021-01-06 11:07:22 +0900

    Pin, Tag model

--- backend/pin/models.py
+++ backend/pin/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Pin(TimestampedModel):
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='pins')
+    title = models.CharField(max_length=30)
+    image = models.ImageField(blank=True, upload_to="pins/%Y/%m/%d")
+    tag_set = models.ManyToManyField(to='Tag', blank=True)
+
+
+# Pin, Tag: Many To Many
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50, blank=True)

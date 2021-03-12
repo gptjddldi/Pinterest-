608e95a75738ff7348617c3898940bba2b744a20
hyeseong <hystom38@gmail.com>
2021-03-12 13:45:18 +0900

    follow button 오류 해결 (follow / unfollow 할 때 redux update 하여 바로 반영되도록)

--- frontend/src/components/FollowButton.js
+++ frontend/src/components/FollowButton.js
@@ -1,30 +1,39 @@
 import React from 'react'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import SecondaryButton from "./Button/SecondaryButton";
 import {axiosInstance} from "../utils/axios";
+import {update} from "../actions/userAction";
 
 export default function FollowButton({user}){
     let [isFollowing, setIsFollowing] = useState(false)
     const {loggedUser} = useSelector(state => ({
         loggedUser: state.userReducer.user,
     }))
+    const dispatch = useDispatch()
+    const onUpdate = (data) => dispatch(update(data));
 
     useEffect(() => {
-        if(loggedUser.following_user.indexOf(user) !== -1){
+        if(loggedUser.following_user.indexOf(user.id) !== -1){
             setIsFollowing(true)
         } else setIsFollowing(false)
-    }, [user])
+    }, [user,loggedUser, isFollowing])
 
     function follow(){
         axiosInstance.post('pinterestAccounts/follow/', {'username': user.username})
-            .then(()=>setIsFollowing(true))
-            .catch((e)=>console.log(e))
+            .then(()=> {
+                setIsFollowing(true)
+                axiosInstance.get('rest-auth/user/').then((res)=>onUpdate(res.data)).catch((e)=>console.log(e.response))
+            })
+            .catch((e)=>console.log(e.response))
     }
     function unfollow(){
         axiosInstance.post('pinterestAccounts/unfollow/', {'username': user.username})
-            .then(()=>setIsFollowing(false))
+            .then(()=> {
+                setIsFollowing(false)
+                axiosInstance.get('rest-auth/user/').then((res)=>onUpdate(res.data)).catch((e)=>console.log(e.response))
+            })
             .catch((e)=>console.log(e))
     }
 

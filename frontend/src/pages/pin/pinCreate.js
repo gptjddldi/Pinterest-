9a92bc949d90079e4cad959a5aa736f6b8a173d6
hyeseong <hystom38@gmail.com>
2021-03-12 12:31:02 +0900

    URL 로 업로드하는 기능 추가

--- frontend/src/pages/pin/pinCreate.js
+++ frontend/src/pages/pin/pinCreate.js
@@ -18,13 +18,20 @@ export default function PinCreate(props){
         e.preventDefault()
         const formData = new FormData();
         formData.append('title', title)
-        formData.append('image', image)
+        if(image)
+            formData.append('image', image)
+        else if(imageURL)
+            formData.append('image_url', imageURL)
         axiosInstance.post('/pins/', formData).then((res)=>history.push(`pin/${res.data.id}`))
             .catch((e)=>console.log(e.response))
     }
     const handleChange = (e) => {
         setImage(e.target.files[0])
         setImageURL(URL.createObjectURL(e.target.files[0]))
+    }
+    const handleUrl = (e) => {
+        setImageURL(e.target.value)
+        // setImage(e.target.value)
     }
     return (
         <Layout props={props}>
@@ -42,7 +49,7 @@ export default function PinCreate(props){
                                         <div className="bg-gray-300 w-64 rounded-lg p-2 flex-grow w-full cursor-pointer" onClick={() => fileInputRef.current.click()}>
                                             <div className="rounded-lg border-2 border-gray-400 border-dashed h-full relative">
                                                 <div className="text-center mx-auto" style={{marginTop: "50%", minHeight: "15rem"}}>
-                                                    <div className="my-2">드래그하거나 클릭하여 업로드</div>
+                                                    <div className="my-2">이미지 업로드</div>
                                                 </div>
                                                 <div className="absolute text-sm bottom-0 text-center">권장사항 : 20MB 이하 고화질 원본 .jpg 파일</div>
                                             </div>
@@ -51,6 +58,8 @@ export default function PinCreate(props){
                                         { imageURL &&
                                         <img src={imageURL} />
                                         }
+                                        이미지 URL 로 업로드 하기
+                                        <input type="text" id='imageURL' placeholder="이미지 URL 입력해보세요!" value={imageURL} onChange={handleUrl}/>
                                     </div>
                                     <div className="sm:px-10 py-5 w-full sm:w-1/2" style={{minHeight: "510px"}}>
                                         <input ref={fileInputRef} type="file" className="hidden" onChange={handleChange}/>
